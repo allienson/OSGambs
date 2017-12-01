76a7acec904a005c2405fd79120ed9742b1bb3ce
Unknown <seabradenucci@hotmail.com>
2017-12-01 19:53:53 -0200

    Escalonador correto

--- despachante.py
+++ despachante.py
@@ -46,42 +46,63 @@ def existe_processo_para_entrar_em_execucao():
 def adicionar_processos_nas_filas_de_execucao(tempo_atual):
     if ((len(processos) > 0)):
         while ((processos[0].tempo_init <= tempo_atual)):
-            if (memoria.memoria_disponivel(processos[0])):
-                fila.adiciona_em_fila(processos.pop(0))
-            else:
-                processos.pop(0)
+            fila.adiciona_em_fila(processos.pop(0))
             if (len(processos) == 0): break
 
 def escalonar():
     global tempo
 
-    if (fila.existe_processo_real()):
-        # for i in range(0,len(fila.processos_real)):
-        #     if(fila.processos_real[i].tempo_decorrido == 0):
-        #         if(memoria.memoria_disponivel(fila.processos_real[i])):
-        #             executa_real(fila.processos_real[i],i)
-        #     else:
-        #         executa_real(fila.processos_real[i],i)
-        executa_real(fila.processos_real[0],0)
-    elif (fila.existe_processo_1()):
-        executa_usuario(fila.processos_usuario1[0], fila.processos_usuario1)
-    elif (fila.existe_processo_2()):
-        executa_usuario(fila.processos_usuario2[0], fila.processos_usuario2)
-    elif (fila.existe_processo_3()):
-        executa_usuario(fila.processos_usuario3[0], fila.processos_usuario3)
-    else:
-        tempo += 1
+    #testa fila real
+    for i in range(0,len(fila.processos_real)):
+        if(fila.processos_real[i].tempo_decorrido == 0):
+            if(memoria.memoria_disponivel(fila.processos_real[i])):
+                executa_real(fila.processos_real.pop(i))
+                return
+        else:
+            executa_real(fila.processos_real.pop(i))
+            return
 
-def executa_real(proc,posicao):
+    #testa a fila de prioridade 1
+    for i in range(0,len(fila.processos_usuario1)):
+        if(fila.processos_usuario1[i].tempo_decorrido == 0):
+            if(memoria.memoria_disponivel(fila.processos_usuario1[i])):
+                executa_usuario(fila.processos_usuario1[i],fila.processos_usuario1,i)
+                return
+        else:
+            executa_usuario(fila.processos_usuario1[i],fila.processos_usuario1,i)
+            return
+
+    #testa a fila de prioridade 2
+    for i in range(0,len(fila.processos_usuario2)):
+        if(fila.processos_usuario2[i].tempo_decorrido == 0):
+            if(memoria.memoria_disponivel(fila.processos_usuario2[i])):
+                executa_usuario(fila.processos_usuario2[i],fila.processos_usuario2,i)
+                return
+        else:
+            executa_usuario(fila.processos_usuario2[i],fila.processos_usuario2,i)
+            return
+
+    #testa a fila de prioridade 3
+    for i in range(0,len(fila.processos_usuario3)):
+        if(fila.processos_usuario3[i].tempo_decorrido == 0):
+            if(memoria.memoria_disponivel(fila.processos_usuario3[i])):
+                executa_usuario(fila.processos_usuario3[i],fila.processos_usuario3,i)
+                return
+        else:
+            executa_usuario(fila.processos_usuario3[i],fila.processos_usuario3,i)
+            return
+    tempo += 1
+
+
+def executa_real(proc):
     global tempo
     for i in range(0, proc.tempo_cpu):
         print("    P" + str(proc.pid) + " instrucao " + str(i + 1))
         #time.sleep(1)
     memoria.libera_memoria_real(proc)
     tempo += proc.tempo_cpu
-    fila.processos_real.pop(posicao)
 
-def executa_usuario(proc, fila):
+def executa_usuario(proc, fila,pos):
     global tempo
     tempo += 1
     proc.tempo_decorrido += 1
@@ -91,7 +112,7 @@ def executa_usuario(proc, fila):
 
     if (proc.tempo_cpu == proc.tempo_decorrido):
         memoria.libera_memoria_usuario(proc)
-        fila.pop(0)
+        fila.pop(pos)
         
 # Imprime os dados de cada processo executado pelo dispachante
 # def imprime_processo(proc):
